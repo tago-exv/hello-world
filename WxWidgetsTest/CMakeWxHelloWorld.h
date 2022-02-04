7a8dfe5ef5bc458d7cad4c9ba5bd2ef7b80d0751
tago-exv <tago@exvision.co.jp>
2022-02-04 11:09:23 +0900

    Create New project

---
+++ WxWidgetsTest/CMakeWxHelloWorld.h
@@ -0,0 +1,27 @@
+// wxWidgets "Hello World" Program
+
+#pragma once
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+
---
+++ WxWidgetsTest/README.md
@@ -0,0 +1,7 @@
+# CMakeWxHelloWorld
+
+Very simple hello world sample code to start a WxWidgets project with CMake.
+
+
+
+
